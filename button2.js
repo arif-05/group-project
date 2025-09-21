345700bbb250be6567586d8e8cde74d5c9d4d6b4
Nazib017 <nazib.ndc119@gmail.com>
2025-09-21 15:46:26 +0600

    Create button2.js
    js

---
+++ button2.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("myButton");
+
+button.addEventListener("click", () => {
+  button.style.backgroundColor = "lightblue"; 
+  button.style.color = "white"; 
+});
